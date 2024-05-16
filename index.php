03ca5a981e4a77c3f84be86e8cc77e0b27e8cd7a
JulijaTaranda <153869026+JulijaTaranda@users.noreply.github.com>
2024-05-16 14:17:16 +0300

    Add files via upload
    1. Pievienota stilizācija
    2. Izslēgti brīdinājumi, kas saistīti ar loadHTML() metodi 
    *( kas nav problēma, ja HTML kods, ko parsējam, ir korekts. Kļūdas rodas, jo DOMDocument mēģina interpretēt HTML kodu, kas var saturēt nepareizus vai nesaderīgus elementus).

--- index.php
+++ index.php
@@ -5,11 +5,15 @@ $url = 'https://rigacoding.lv';
 $html = file_get_contents($url);
 //echo $html; //for test
 
+libxml_use_internal_errors(true); // temporerily disabling error output
+
 //create object, load  html code into object
 $dom = new DOMDocument();
 $dom->loadHTML($html);
 
-//Course name is in tag <a></a> inside tag <h3></h3>
+libxml_use_internal_errors(false); // re-enabling error output
+
+//course name is in tag <a></a> inside tag <h3></h3>
 $courseElements = $dom->getElementsByTagName('h3');
 
 $courses = [];
@@ -20,7 +24,8 @@ foreach ($courseElements as $element) {
         // Get "text content" from link
         $courseName = $link->nodeValue;
         $courses[] = $courseName;
-    } /*else {
+    } /*for test
+    else {
         echo "Cannot find link in tag h3:\n";
         echo $element->ownerDocument->saveHTML($element) . "\n";
     }*/
@@ -37,17 +42,17 @@ foreach ($courseElements as $element) {
     <title>Web Scraper</title>
 </head>
 <body>
-    <h3>Riga Coding School piedāvātie kursi</h3>
-    <table border="1">
+    <table border="1" style="border-collapse: collapse; margin: 40px auto;">
+    <caption><h3>Riga Coding School piedāvātie kursi</h3></caption>
         <tr>
-            <th>Nr.</th>
-            <th>Kursa nosaukums</th>
+            <th style="padding: 8px; background-color: #f2f2f2; font-weight: bold;">Nr.</th>
+            <th style="padding: 8px; background-color: #f2f2f2; font-weight: bold;">Kursa nosaukums</th>
         </tr>
         <?php $count = 1; ?>
         <?php foreach ($courses as $course) : ?>
            <tr>
-              <td><?= $count++; ?></td>
-              <td><?= $course; ?></td> 
+              <td style="padding: 8px; font-weight: bold;"><?= $count++; ?></td>
+              <td style="padding: 8px;"><?= $course; ?></td> 
            </tr>
         <?php endforeach; ?>
     </table>
